1542609dd8774d248f411ed454a0534264ebe172
amy-wu <amywu177@gmail.com>
2024-05-19 18:43:34 +1000

    3D page

--- src/3DPage.jsx
+++ src/3DPage.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import './3DPage.css'
 
+import CoverImage3D from './assets/Images/3DCoverWide.png';
+
 import AF_1 from './assets/3DGalleryRenders/AF_1.png';
 import AF_2 from './assets/3DGalleryRenders/AF_2.png';
+import AM_1 from './assets/3DGalleryRenders/AM_1.png';
+import AM_2 from './assets/3DGalleryRenders/AM_2.png';
+
 import NM_1 from './assets/3DGalleryRenders/NM_1.png';
 import NM_2 from './assets/3DGalleryRenders/NM_2.png';
+import NF_1 from './assets/3DGalleryRenders/NF_1.png';
+import NF_2 from './assets/3DGalleryRenders/NF_2.png';
 
 function ThreeDCollection(){
 	const presets = [
+		{id:4, name:'Necromancer Female', images:[NF_1,NF_2]},
+		{id:3, name:'Necromancer Male', images:[NM_1,NM_2]},
 		{id:1, name:'Assasin Female', images:[AF_1,	AF_2]},
-		{id:2, name:'Necromancer Male', images:[NM_1,NM_2]}
+		{id:2, name:'Assasin Male', images:[AM_1,AM_2]}
+
 	];
 	
 	//An array to store the names of the first image of each object
@@ -28,9 +38,10 @@ function ThreeDCollection(){
 	return(
         <>
 			<div className="backgroundColor">
-				<div className="galleryContainer">
-						{imageElements}
-				</div>
+				<img className="displayImage3D" src={CoverImage3D}></img>
+					<div className="galleryContainer">
+							{imageElements}
+					</div>
 			</div>
         </>
     );
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,13 +3,13 @@ import './MainPage.css'
 import Navbar from './assets/Components/NavBar/Navbar.jsx';
 import { Link } from "react-router-dom";
 
-function handleMouseEnter(event, text){
+function handleMouseEnter(event, text) {
   const target = event.target;
   const div = document.querySelector('.hoverText');
   div.textContent = text;
 }
 
-function handleMouseLeave(event){
+function handleMouseLeave(event) {
   const target = event.target;
   const div = document.querySelector('.hoverText');
   div.textContent = '';
@@ -17,28 +17,29 @@ function handleMouseLeave(event){
 
 function App() {
   return (
-      <>
-          <Navbar />
-          <div className = 'mainPageColor'>
-            <div className='pageContainer'>
-                <div className='frontPageContainer'>
-                    <Link to="/2Dpage"><img
-                          className="frontPageSelection"
-                          src="src/assets/Images/2DFont.svg"
-                          onMouseEnter={(e) => handleMouseEnter(e, 'Illustrations, Concept Design (Character, Creature + Assets), Animation')}
-                          onMouseLeave={handleMouseLeave}
-              /></Link>
-                    <Link to="/3Dpage"><img
-                          className="frontPageSelection"
-                          src="src/assets/Images/3DFont.svg"
-                          onMouseEnter={(e) => handleMouseEnter(e, 'Low Poly Modelling, High Poly Sculpting, Texture Painting, Animation + fx')}
-                          onMouseLeave={handleMouseLeave}
-              /></Link>
-                </div>
-            </div>
-            <div className="hoverText"></div>
+    <>
+
+      <div className='mainPageColor'>
+      <Navbar />
+        <div className='pageContainer'>
+          <div className='frontPageContainer'>
+            <Link to="/2Dpage"><img
+              className="frontPageSelection"
+              src="src/assets/Images/2DFont.svg"
+              onMouseEnter={(e) => handleMouseEnter(e, 'Illustrations, Concept Design (Character, Creature + Assets), Animation')}
+              onMouseLeave={handleMouseLeave}
+            /></Link>
+            <Link to="/3Dpage"><img
+              className="frontPageSelection"
+              src="src/assets/Images/3DFont.svg"
+              onMouseEnter={(e) => handleMouseEnter(e, 'Low Poly Modelling, High Poly Sculpting, Texture Painting, Animation + fx')}
+              onMouseLeave={handleMouseLeave}
+            /></Link>
           </div>
-      </>
+        </div>
+        <div className="hoverText"></div>
+      </div>
+    </>
   );
 }
 
