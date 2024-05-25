1ec6bfbe0a5d84f00aa0137fe85a65fe2a3cc2e4
amy-wu <amywu177@gmail.com>
2024-05-26 00:06:10 +1000

    Updated assets folder

--- src/3DPage.jsx
+++ src/3DPage.jsx
@@ -5,37 +5,39 @@ import CoverImage3D from './assets/Images/3DCoverWide.png';
 import leftArrow from './assets/Images/arrow_button_left.png';
 import rightArrow from './assets/Images/arrow_button_right.png';
 
-import * as GalleryImages from './assets/3DGalleryRenders/imageExport';
+import * as GalleryImages from './assets/3DGalleryRenders/Characters/imageExport';
 import Navbar from './assets/Components/NavBar/Navbar';
 
 function ThreeDCollection(){
-	const presets = [
+	const presetsOfCharacters = [
 		{id:4, name:'Necromancer Female', images:[GalleryImages.NF_1,GalleryImages.NF_2,GalleryImages.NF_4,GalleryImages.NF_3]},
 		{id:3, name:'Necromancer Male', images:[GalleryImages.NM_1,GalleryImages.NM_2,GalleryImages.NM_3,GalleryImages.NM_4]},
-		{id:1, name:'Assasin Female', images:[GalleryImages.AF_1,GalleryImages.AF_2,GalleryImages.AF_3,GalleryImages.AF_4]},
-		{id:2, name:'Assasin Male', images:[GalleryImages.AM_1,GalleryImages.AM_2,GalleryImages.AM_3,GalleryImages.AM_4]},
+		{id:1, name:'Ashashin Female', images:[GalleryImages.AF_1,GalleryImages.AF_2,GalleryImages.AF_3,GalleryImages.AF_4]},
+		{id:2, name:'Azhasheen Male', images:[GalleryImages.AM_1,GalleryImages.AM_2,GalleryImages.AM_3,GalleryImages.AM_4]},
 		{id: 6, name: 'Mercenary Male', images: [GalleryImages.MeM_1, GalleryImages.MeM_2, GalleryImages.MeM_3, GalleryImages.MeM_4] },
-		{id: 5, name: 'Mercenary Female', images: [GalleryImages.MeF_1, GalleryImages.MeF_2, GalleryImages.MeF_3, GalleryImages.MeF_4] }
+		{id: 5, name: 'Mercenary Female', images: [GalleryImages.MeF_1, GalleryImages.MeF_2, GalleryImages.MeF_3, GalleryImages.MeF_4] },
+		{ id: 7, name: 'BF_1', images: [GalleryImages.BF_1, GalleryImages.BF_2, GalleryImages.BF_3, GalleryImages.BF_4] },
+    	{ id: 8, name: 'BM_1', images: [GalleryImages.BM_1, GalleryImages.BM_2, GalleryImages.BM_3, GalleryImages.BM_4] }
 	];
 	
 	//An array to store the names of the first image of each object
-	const displayImages = presets.map(presets => presets.images[0]);
+	const displayImages = presetsOfCharacters.map(presetsOfCharacters => presetsOfCharacters.images[0]);
 	
-	//Tracks currently selected object in the presets array
+	//Tracks currently selected object in the presetsOfCharacters array
 	const [presetIndex, setPresetIndex] = useState(0);
 
-	//Tracks currently displayed image within the selected presets' images array
+	//Tracks currently displayed image within the selected presetsOfCharacters' images array
     const [imageIndex, setImageIndex] = useState(0);
 	
 	//Click on an image to enlarge it in a modal form
 	const [modalImage, setModalImage] = useState(null);
 
 	const handleClick = (presetId) => {
-		const selectedPreset = presets.find(p => p.id === presetId);
+		const selectedPreset = presetsOfCharacters.find(p => p.id === presetId);
 		console.log("Selected Preset:", selectedPreset);
 
 		if (selectedPreset) {
-			setPresetIndex(presets.indexOf(selectedPreset)); // Find the index of the selected preset
+			setPresetIndex(presetsOfCharacters.indexOf(selectedPreset)); // Find the index of the selected preset
         	setImageIndex(0); // Reset image index when a new preset is clicked
 			setModalImage(selectedPreset.images[0]); // Accessing the images property of the selectedPreset
 		}
@@ -56,7 +58,7 @@ function ThreeDCollection(){
 				<img className = "images" 
 				key={i} 
 				src={displayImages[i]}
-				onClick={() => handleClick(presets[i].id)}
+				onClick={() => handleClick(presetsOfCharacters[i].id)}
 				></img>
 			);
 		}
@@ -69,14 +71,14 @@ function ThreeDCollection(){
 					setImageIndex(prevIndex => 
 						{
 							const newIndex = prevIndex === 0 ? prevIndex : prevIndex - 1;		//checks if prevIndex = 0? if 'true' : if 'false'
-							setModalImage(presets[presetIndex].images[newIndex]);				//updates Modal, the 'display' image
+							setModalImage(presetsOfCharacters[presetIndex].images[newIndex]);				//updates Modal, the 'display' image
 							return newIndex;													//returns newIndex so the previous line will update correctly
 						});
 					
 				} else if (event.key === "ArrowRight") {
 					setImageIndex(prevIndex => {
-							const newIndex = prevIndex === presets[presetIndex].images.length - 1 ? prevIndex : prevIndex + 1;
-							setModalImage(presets[presetIndex].images[newIndex]);
+							const newIndex = prevIndex === presetsOfCharacters[presetIndex].images.length - 1 ? prevIndex : prevIndex + 1;
+							setModalImage(presetsOfCharacters[presetIndex].images[newIndex]);
 							return newIndex;
 					});
 				}
@@ -89,21 +91,21 @@ function ThreeDCollection(){
 			return () => {
 				document.body.removeEventListener('keydown', handleKeyDown);
 			};
-		}, [presetIndex, presets]);
+		}, [presetIndex, presetsOfCharacters]);
 
 		//Cycle through the .images[index] with <- and -> arrow buttons
 		const leftArrowClick = () => {
 			setImageIndex(prevIndex => {
 			const newIndex = prevIndex === 0 ? prevIndex : prevIndex - 1;
-			setModalImage(presets[presetIndex].images[newIndex]);
+			setModalImage(presetsOfCharacters[presetIndex].images[newIndex]);
 			return newIndex;
 			});
 		};
 
 		const rightArrowClick = () => {
 			setImageIndex(prevIndex => {
-				const newIndex = prevIndex === presets[presetIndex].images.length - 1 ? prevIndex : prevIndex + 1;
-				setModalImage(presets[presetIndex].images[newIndex]);
+				const newIndex = prevIndex === presetsOfCharacters[presetIndex].images.length - 1 ? prevIndex : prevIndex + 1;
+				setModalImage(presetsOfCharacters[presetIndex].images[newIndex]);
 				return newIndex;
 			});
 		};
@@ -113,12 +115,11 @@ function ThreeDCollection(){
 		{/* Gallery images display */}
 			<div className="backgroundColor">
 				<Navbar/>
-				<div>
+				<div className='galleryImages'>
 					<img className="displayImage3D" src={CoverImage3D}></img>
-						<div className="galleryContainer">
-								{imageElements}
-						</div>
-
+					{imageElements}
+						
+				</div>
 		{/* Modal image display and interaction */}
 				
 				{modalImage && (
@@ -130,7 +131,7 @@ function ThreeDCollection(){
 						</button>
 					)}
 					
-					{imageIndex < presets[presetIndex].images.length - 1 && ( 
+					{imageIndex < presetsOfCharacters[presetIndex].images.length - 1 && ( 
 						<button className='rightArrowButton' onClick={rightArrowClick}>
 							<img src = {rightArrow} className='arrowButtonImage'></img>
 						</button>
@@ -144,7 +145,7 @@ function ThreeDCollection(){
 				</>
 			)}
 				</div>
-			</div>
+			
 
 		
 			
