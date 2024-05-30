6e12cc1bf5027724f90f89aa90fcbf2b9101a6c2
amy-wu <amywu177@gmail.com>
2024-05-30 14:23:57 +1000

    Context Provider
    added in NavBar

--- src/3DPage.jsx
+++ src/3DPage.jsx
@@ -4,30 +4,40 @@ import React, { useEffect, useState } from "react";
 import CoverImage3D from './assets/Images/3DCoverWide.png';
 import leftArrow from './assets/Images/arrow_button_left.png';
 import rightArrow from './assets/Images/arrow_button_right.png';
-
-import * as GalleryImages from './assets/3DGalleryRenders/Characters/imageExport';
 import Navbar from './assets/Components/NavBar/Navbar';
+import * as Char3D from './assets/3DGalleryRenders/Characters/imageExport.js';
+import * as Obj3D from './assets/3DGalleryRenders/Objects/objectImport.js';
+import iceEnviron from './assets/3DGalleryRenders/Environments/Ice.png';
 
 function ThreeDCollection(){
-	const presetsOfCharacters = [
-		{id:4, name:'Eldritch Witch', images:[GalleryImages.NF_1,GalleryImages.NF_2,GalleryImages.NF_4,GalleryImages.NF_3]},
-		{id:3, name:'Eldritch Warlock', images:[GalleryImages.NM_1,GalleryImages.NM_2,GalleryImages.NM_3,GalleryImages.NM_4]},
-		{id:1, name:'Desert Tribes Assassin Female', images:[GalleryImages.AF_1,GalleryImages.AF_2,GalleryImages.AF_3,GalleryImages.AF_4]},
-		{id:2, name:'Desert Tribes Assassin Male', images:[GalleryImages.AM_1,GalleryImages.AM_2,GalleryImages.AM_3,GalleryImages.AM_4]},
-		{id: 6, name: 'Dark Woods Warrior', images: [GalleryImages.MeM_1, GalleryImages.MeM_2, GalleryImages.MeM_3, GalleryImages.MeM_4] },
-		{id: 5, name: 'Dark Woods Archer', images: [GalleryImages.MeF_1, GalleryImages.MeF_2, GalleryImages.MeF_3, GalleryImages.MeF_4] },
-		{ id: 7, name: 'Dwarf Pirate', images: [GalleryImages.BF_1, GalleryImages.BF_2, GalleryImages.BF_3, GalleryImages.BF_4] },
-    	{ id: 8, name: 'Minotaur Pirate', images: [GalleryImages.BM_1, GalleryImages.BM_2, GalleryImages.BM_3, GalleryImages.BM_4] }
-	];
+
+	    //3D Gallery Characters
+		const presetsOfCharacters = [
+			{id:4, name:'Eldritch Witch', images:[Char3D.NF_1,Char3D.NF_2,Char3D.NF_4,Char3D.NF_3]},
+			{id:3, name:'Eldritch Warlock', images:[Char3D.NM_1,Char3D.NM_2,Char3D.NM_3,Char3D.NM_4]},
+			{id:1, name:'Desert Tribes Assassin Female', images:[Char3D.AF_1,Char3D.AF_2,Char3D.AF_3,Char3D.AF_4]},
+			{id:2, name:'Desert Tribes Assassin Male', images:[Char3D.AM_1,Char3D.AM_2,Char3D.AM_3,Char3D.AM_4]},
+			{id: 6, name: 'Dark Woods Warrior', images: [Char3D.MeM_1, Char3D.MeM_2, Char3D.MeM_3, Char3D.MeM_4] },
+			{id: 5, name: 'Dark Woods Archer', images: [Char3D.MeF_1, Char3D.MeF_2, Char3D.MeF_3, Char3D.MeF_4] },
+			{ id: 7, name: 'Dwarf Pirate', images: [Char3D.BF_1, Char3D.BF_2, Char3D.BF_3, Char3D.BF_4] },
+			{ id: 8, name: 'Minotaur Pirate', images: [Char3D.BM_1, Char3D.BM_2, Char3D.BM_3, Char3D.BM_4] }
+		];
 	
-	const presetOfObjects = [
-		{ id: 1, name: 'Crab Lantern', images: [GalleryImages.C_1, GalleryImages.C_2, GalleryImages.C_3, GalleryImages.C_W] },
-		{ id: 2, name: 'Eel Lamp', images: [GalleryImages.E_1, GalleryImages.E_2, GalleryImages.E_3, GalleryImages.E_W] },
-		{ id: 3, name: 'Jellyfish Compass', images: [GalleryImages.J_1, GalleryImages.J_2, GalleryImages.J_3, GalleryImages.J_W] },
-		{ id: 4, name: 'Shark Lamp', images: [GalleryImages.S_1, GalleryImages.S_2, GalleryImages.S_3, GalleryImages.S_W] },
-		{ id: 5, name: 'Octopus Bottle', images: [GalleryImages.T_1, GalleryImages.T_2, GalleryImages.T_3, GalleryImages.T_W] },
-		{ id: 6, name: 'Faberge Egg', images: [GalleryImages.Egg_1, GalleryImages.Egg_W] }
-	];
+		//3D Gallery Objects
+		const presetOfObjects = [
+			{ id: 1, name: 'Crab Lantern', images: [Obj3D.C_1, Obj3D.C_2, Obj3D.C_3, Obj3D.C_W] },
+			{ id: 2, name: 'Eel Lamp', images: [Obj3D.E_1, Obj3D.E_2, Obj3D.E_3, Obj3D.E_W] },
+			{ id: 3, name: 'Jellyfish Compass', images: [Obj3D.J_1, Obj3D.J_2, Obj3D.J_3, Obj3D.J_W] },
+			{ id: 4, name: 'Shark Lamp', images: [Obj3D.S_1, Obj3D.S_2, Obj3D.S_3, Obj3D.S_W] },
+			{ id: 5, name: 'Octopus Bottle', images: [Obj3D.T_1, Obj3D.T_2, Obj3D.T_3, Obj3D.T_W] },
+			{ id: 6, name: 'Faberge Egg', images: [Obj3D.Egg_1, Obj3D.Egg_W] }
+		];
+	
+		//3D Gallery Environments
+		const presetOfEnvironments = [
+			{ id: 1, name: 'Frosted Cave', images: [iceEnviron] }
+		];
+	
 	//An array to store the names of the first image of each object
 	const displayImages = presetsOfCharacters.map(presetsOfCharacters => presetsOfCharacters.images[0]);
 	
--- src/assets/Components/NavBar/Navbar.jsx
+++ src/assets/Components/NavBar/Navbar.jsx
@@ -1,13 +1,35 @@
 import './Navbar.css'
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, createContext, useContext} from "react";
 import { Link, useLocation } from "react-router-dom";
 import homeIcon from "../../Images/home_icon.png";
 
+//preset selection for 3D page using context and provider
+    //Provides a way to pass 'global' data without having to pass it down manually at every level
+    export const PresetContext = createContext();
+
+    //Wraps a portion, creates a <></> component to make the context avaliable
+    export const PresetProvider = ({children}) => {
+        const [selectPreset, setSelectPreset] = useState(presetsOfCharacters);
+        const selectNew = (preset) => {setSelectPreset(preset);};
+
+    return(
+        <PresetContext.Provider value={{selectPreset, selectNew}}>
+            {children}
+        </PresetContext.Provider>
+        );
+    }
+
 const Navbar = () => {
+
     const location = useLocation();
+
+    //consuming the above context
+    const { selectPreset, selectNew } = useContext(PresetContext);
 
     return (
         <>
+    <PresetProvider>
+        {/* navBar component */}
             <nav>
                 {/* Left NavBar icon */}
                 <div className='NavIcon'>
@@ -20,16 +42,28 @@ const Navbar = () => {
                     {location.pathname.toLowerCase() === "/2Dpage" && <li>2D Page</li>}
                     {location.pathname.toLowerCase() === "/3dpage" &&
                         <>
-                            <button>CHARACTERS</button>
+                            <button onClick={() => {
+                                setSelectPreset(presetsOfCharacters),
+                                console.log(`${selectPreset} is selected`);
+                                }}>CHARACTERS</button>
                             <button>OBJECTS</button>
                             <button>ENVIRONMENT</button>
-                        </>}
+                        </>
+                    }
                 </div>
-
-
             </nav>
+            </PresetProvider>
         </>
     )
 }
 
-export default Navbar
+//Wrapping the Navbar with the context provider to ensure that it always has the right context
+const App = () => {
+    return (
+        <PresetProvider> 
+            <Navbar />
+        </PresetProvider>
+    );
+};
+
+export default App;
